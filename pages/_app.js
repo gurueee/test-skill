0a7a5f122557c14e39cb5006e750e3919d27e0bf
gurueee <gurueee@gmail.com>
2026-04-10 14:19:49 +0530

    del

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css';
-import { ThemeProvider } from '../lib/theme';
-export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-}
